(among other things) added rectified linear units and dropout.  correct operation should be verified

models/mlp.py
@@ -15,7 +15,7 @@ import pdb
 from matplotlib import pyplot as plt
 
 class mlp:
-	def __init__(self, layers, rng_seed=None):		
+	def __init__(self, layers, act_type='sigmoid', rng_seed=None):		
 		'''
 		layers is a list specifying the dimension of each network layer:
 			layers = [#n_visible, #n_hidden1, #n_hidden2, ...]
@@ -24,8 +24,15 @@ class mlp:
 			layers = [20,10,2]		
 		implements binary classification with inputs of dimension 20, and 10 units in the first hidden layer
 		'''
+		self.act_types = {'sigmoid':0, 'tanh':1, 'relu':2}
+
 		if rng_seed:
 			np.random.seed(rng_seed)
+
+		if not act_type in self.act_types:
+			raise Exception("Unknown activation type.  Currently recognized options are %s" % self.act_types)
+		else:
+			self.act_type = act_type
 
 		self.layers = layers
 		self.n_hid  = len(layers) - 1 # number of hidden layers				
@@ -40,16 +47,34 @@ class mlp:
 		self.act0   =  None
 		
 		self.dE     = self.n_hid * [None]		
+		
+		self.W_inc  = self.n_hid * [None]
+		self.b_inc  = self.n_hid * [None]
 
+		self.weight_decay = 0	
+			
 		for k in xrange(self.n_hid):
-			self.W[k] = 1e-2 * np.random.standard_normal( (layers[k], layers[k+1]) )
-			self.b[k] = 1e-6 * np.random.standard_normal( (1, layers[k+1]) ) #np.zeros( (1,layers[k+1]) )
+			self.W[k]     = 1e-1 * np.random.standard_normal( (layers[k], layers[k+1]) )
+			self.b[k]     = np.zeros( (1,layers[k+1]) )
 
-	def sigmoid(self, x):
-		return 1. / (1 + np.exp(-x))
+			self.W_inc[k] = np.zeros( self.W[k].shape )
+			self.b_inc[k] = np.zeros( self.b[k].shape )
 
-	def sigmoid_derivative(self, x):
-		return self.sigmoid(x) * (1 - self.sigmoid(x))
+	def activation_fn(self, x):
+		if self.act_type=='sigmoid':
+			return 1. / (1 + np.exp(-x))
+		elif self.act_type=='tanh':
+			return np.tanh(x)
+		elif self.act_type=='relu':
+			return x * (x>0) # ReLu
+
+	def activation_derivative(self, x):
+		if self.act_type=='sigmoid':
+			return self.activation_fn(x) * (1 - self.activation_fn(x))
+		elif self.act_type=='tanh':
+			return 1 - np.tanh(x)**2
+		elif self.act_type=='relu':
+			return x>0 # ReLu
 
 	def softmax(self, x):
 		# tbd: add overflow protection...
@@ -63,14 +88,17 @@ class mlp:
 			Z = np.sum( np.exp(xc), axis=1 )
 			return np.exp(xc) / Z.reshape( (xc.shape[0],1) )
 
-	def f_prop(self, x):
+	def f_prop(self, x, mask=None):
+		if not mask:
+			mask = np.ones(self.n_hid)
+
 		n_examples  = x.shape[0]
-		self.act0   = np.copy(x) # input activation
+		self.act0   = np.copy(x) * mask[0] # input activation
 		self.pot[0] = np.dot(self.act0, self.W[0]) + np.repeat(self.b[0], n_examples, axis=0) # neuron potential
 
 		# forward propogate input through hidden layers
 		for k in xrange(0,self.n_hid-1):						
-			self.act[k]   = self.sigmoid( self.pot[k] ) # neuron activation
+			self.act[k]   = self.activation_fn( self.pot[k] ) * mask[k+1] # neuron activation
 			self.pot[k+1] = np.dot(self.act[k], self.W[k+1]) + np.repeat(self.b[k+1], n_examples, axis=0) # neuron potential
 
 		# output layer w/ softmax unit for multi-class classification
@@ -83,16 +111,33 @@ class mlp:
 		
 		# back propogate errors
 		for k in xrange(self.n_hid-1,0,-1):
-			self.dE[k-1] = np.dot( self.dE[k], self.W[k].T ) * self.sigmoid_derivative( self.pot[k-1] )			
+			self.dE[k-1] = np.dot( self.dE[k], self.W[k].T) * self.activation_derivative( self.pot[k-1] )			
 
 		# calculate parameter derivatives
-		n_examples = self.act0.shape[0]
-		self.dW[0] = np.dot( self.act0.T, self.dE[0] ) / n_examples
-		self.db[0] = np.sum( self.dE[0], axis=0 ) / n_examples
+		self.dW[0] = np.dot( self.act0.T, self.dE[0] )
+		self.db[0] = np.sum( self.dE[0], axis=0 )
 
 		for k in xrange(1, self.n_hid):
-			self.dW[k] = np.dot( self.act[k-1].T, self.dE[k] ) / n_examples
-			self.db[k] = np.sum( self.dE[k], axis=0 ) / n_examples
+			self.dW[k] = np.dot( self.act[k-1].T, self.dE[k] )
+			self.db[k] = np.sum( self.dE[k], axis=0 )
+
+	def f_prop_drop(self, x, drop_rates=None):
+		if not drop_rates:
+			drop_rates = np.zeros(self.n_hid)
+
+		n_examples  = x.shape[0]
+		self.act0   = np.copy(x) * (1-drop_rates[0]) # input activation
+		self.pot[0] = np.dot(self.act0, self.W[0]) + np.repeat(self.b[0], n_examples, axis=0) # neuron potential
+
+		# forward propogate input through hidden layers
+		for k in xrange(0,self.n_hid-1):						
+			self.act[k]   = self.activation_fn( self.pot[k] ) * (1-drop_rates[k+1]) # neuron activation
+			self.pot[k+1] = np.dot(self.act[k], self.W[k+1]) + np.repeat(self.b[k+1], n_examples, axis=0) # neuron potential
+
+		# output layer w/ softmax unit for multi-class classification
+		self.act[-1] = self.softmax( self.pot[-1] )
+
+		return self.act[-1]
 
 	def cost(self, target):
 		output     = np.copy( self.act[-1] )
@@ -104,49 +149,75 @@ class mlp:
 		for n,k in enumerate(target):
 			c += -np.log(output[n, k])
 			dC[n,k] -= 1
-		#c /= n_examples
+		
+		cost = c/n_examples + self.weight_decay * self.l2_penalty()
+		return cost, dC/n_examples
 
-		return c, dC
+	def l2_penalty(self):
+		penalty = 0
+		for k in xrange(self.n_hid):
+			penalty += np.sum(self.W[k]**2)
+			penalty += np.sum(self.b[k]**2)
+		return 0.5 * penalty
 
-	def grad(self, data, target):		
-		self.f_prop(data)
+	def l1_penalty(self):
+		penalty = 0
+		for k in xrange(self.n_hid):
+			penalty += np.sum(np.abs(self.W[k]))
+		return 0.5 * penalty
+
+	def grad(self, data, target, drop_rates=None):		
+		if drop_rates:
+			mask = self.n_hid * [None]
+			n_examples = data.shape[0]
+			for k in xrange(self.n_hid):
+				mask[k] = np.random.binomial( 1, 1-drop_rates[k], (n_examples, self.layers[k]) )
+		else:
+			mask = None
+
+		self.f_prop(data, mask)
 		self.b_prop(target)
 
-	def train(self, data, target, batch_size=1, learning_rate=1e-1, epochs=10, momentum=0, weight_decay=0):
-		n_batches = data.shape[0] / batch_size
-
-		W_inc = self.n_hid * [None]
-		b_inc = self.n_hid * [None]
-
 		for k in xrange(self.n_hid):
-			W_inc[k] = np.zeros( self.W[k].shape )
-			b_inc[k] = np.zeros( self.b[k].shape )
-		
+			self.dW[k] += self.weight_decay * self.W[k]
+			#self.db[k] += self.weight_decay * self.b[k].flatten()
+		#return self.db, self.dW
+
+	def train(self, data, target, batch_size=1, learning_rate=1e-1, epochs=10, momentum=0, weight_decay=0, drop_rates=None, verbose=False):
+		n_batches = data.shape[0] / batch_size
+		self.weight_decay = weight_decay
+
 		error  = []
-		for epoch in xrange(epochs):			
-			
+		for epoch in xrange(epochs):
+			epoch_cost = 0				
 			for n in xrange(n_batches):
-				index = np.arange(n * batch_size, (n + 1) * batch_size, dtype='int')
-				self.grad(data[index], target[index])								
+				index = np.arange(n * batch_size, (n + 1) * batch_size, dtype='int')			
+				self.grad(data[index], target[index], drop_rates)				
 
 				for k in xrange(self.n_hid):
-					W_inc[k] = momentum * W_inc[k] + learning_rate * (self.dW[k] + weight_decay*self.W[k])
-					b_inc[k] = learning_rate * self.db[k] + momentum * b_inc[k]
+					self.W_inc[k] *= momentum
+					self.b_inc[k] *= momentum
 
-					self.W[k] -= W_inc[k]
-					self.b[k] -= b_inc[k]
+					self.W_inc[k] += (1-momentum) * learning_rate * (self.dW[k])# + weight_decay * self.W[k])
+					self.b_inc[k] += (1-momentum) * learning_rate * (self.db[k])# + weight_decay * self.b[k] / float(n_batches))
+
+					self.W[k]     -= self.W_inc[k]
+					self.b[k]     -= self.b_inc[k]
 
 				cost, _ = self.cost(target[index])
-				error.append(cost)
+				epoch_cost += cost
+			
+			error.append(epoch_cost)
 
-				#print "epoch %d/%d, [%d/%d], cost=%f" % (epoch+1, epochs, n+1, n_batches, cost)
+			if verbose:
+				print "epoch %d/%d, cost=%f" % (epoch+1, epochs, epoch_cost / n_batches)
 
 		return np.sum(error)
 
-	def classify(self, data):
-		act = self.f_prop(data)
+	def classify(self, data, drop_rates=None):
+		act = self.f_prop_drop(data, drop_rates)
 		return np.argmax( act, axis=1 )
-		
+	
 def test_gradient():	
 	'''
 	A script to verify the back propagation implementation by comparison with numerical gradients
@@ -155,6 +226,7 @@ def test_gradient():
 	n_examples  = 100
 	test_data   = np.random.standard_normal( (n_examples, nnet.layers[0]) )
 	test_target = np.random.randint(0, nnet.layers[-1], n_examples)
+	nnet.weight_decay = 1e-1
 
 	# numerical estimate of gradient
 	epsilon = 1e-6	
@@ -213,6 +285,45 @@ def test_gradient():
  	print 'W: The difference between the numerical and back propagation gradients is (should be 0): %f' % delta_W
  	print 'b: The difference between the numerical and back propagation gradients is (should be 0): %f' % delta_b
 
+def test_xor():
+	n_classes = 2
+	n_visible = 2
+	nnet = mlp([n_visible, 2, n_classes], rng_seed=None)
+
+	n_examples = 1000
+
+	train_data = np.random.randint(0,n_classes,(n_examples,n_visible))
+	train_target = np.array(np.sum(train_data, axis=1)==1, dtype='int')
+
+	test_data = np.random.randint(0,n_classes,(n_examples,n_visible))
+	test_target = np.array(np.sum(test_data, axis=1)==1, dtype='int')
+
+	nnet.train(train_data, train_target, batch_size=10, learning_rate=1e-1, epochs=200, momentum=0.5, verbose=True)
+
+	test_labels = nnet.classify(test_data)
+	percent_error = np.sum(np.abs(test_labels - test_target)>0) / float(n_examples) * 100	
+	print 'Error on test set is: %f percent' % percent_error
+
+def test_xor_dropout():
+	n_classes = 2
+	n_visible = 2
+	nnet = mlp([n_visible, 50, n_classes], act_type='relu', rng_seed=None)
+
+	n_examples = 1000
+
+	train_data = np.random.randint(0,n_classes,(n_examples,n_visible))
+	train_target = np.array(np.sum(train_data, axis=1)==1, dtype='int')
+
+	test_data = np.random.randint(0,n_classes,(n_examples,n_visible))
+	test_target = np.array(np.sum(test_data, axis=1)==1, dtype='int')
+
+	drop_rates=[0.5,0.5]
+	nnet.train(train_data, train_target, batch_size=10, learning_rate=1e-1, epochs=200, momentum=0.5, drop_rates=drop_rates, verbose=True)
+
+	test_labels = nnet.classify(test_data, drop_rates)
+	percent_error = np.sum(np.abs(test_labels - test_target)>0) / float(n_examples) * 100	
+	print 'Error on test set is: %f percent' % percent_error
+
 def test_mlp():
 	'''
 	A simple test of the MLP using synthetic data
@@ -260,16 +371,70 @@ def test_mlp():
 	print [(i,j) for i,j in zip(test_target, test_labels)]
 
 	# report classification error
-	per_error = np.sum(np.abs(test_labels - test_target)>0) / float(n_examples) * 100	
-	print 'Error on test set is: %f percent' % per_error
+	percent_error = np.sum(np.abs(test_labels - test_target)>0) / float(n_examples) * 100	
+	print 'Error on test set is: %f percent' % percent_error
 
 	#pdb.set_trace()
+
+def test_mlp_dropout():
+	'''
+	A simple test of the MLP using synthetic data
+	'''
+	n_classes    = 2
+	n_visible    = 50
+	nnet         = mlp([n_visible,500,500,500,n_classes], act_type='relu', rng_seed=1234)		
+
+	# generate some synthetic train/test data
+	# np.random.seed(1234)
+	n_examples   = n_classes*100
+
+	train_data   = np.zeros( (n_examples, n_visible) )
+	train_target = np.zeros( n_examples, dtype='int' )
+	
+	test_data    = np.zeros( (n_examples, n_visible) )
+	test_target  = np.zeros( n_examples, dtype='int' )
+
+	for c in xrange(n_classes):		
+		# choose random basis vector		
+		s  = 0.1
+		s2 = 2*s
+		vec = s + (1-2*s)*np.random.rand(1,n_visible)
+
+		for n in xrange(n_examples/n_classes):
+			train_data[c + n*n_classes, :] = vec + s2*np.random.standard_normal(n_visible)
+			train_target[c + n*n_classes]  = c
+			
+			test_data[c + n*n_classes, :]  = vec + s2*np.random.standard_normal(n_visible)
+			test_target[c + n*n_classes]   = c
+
+	# shuffle training data
+	perm         = np.random.permutation(n_examples)
+	train_data   = train_data[perm]
+	train_target = train_target[perm]	
+
+	# train
+	batch_size = n_examples/n_classes
+	drop_rates = [0.,0.25,0.25,0.25] 
+	nnet.train(train_data, train_target, batch_size, learning_rate=1e-2, epochs=100, momentum=0.5, drop_rates=drop_rates, verbose=True)
+	# plt.plot(nnet.error)
+
+	# test
+	test_labels = nnet.classify(test_data, drop_rates=drop_rates)
+
+	print [(i,j) for i,j in zip(test_target, test_labels)]
+
+	# report classification error
+	percent_error = np.sum(np.abs(test_labels - test_target)>0) / float(n_examples) * 100	
+	print 'Error on test set is: %f percent' % percent_error
 
 # -------------------------
 if __name__ == '__main__':
 
 	# verify gradients
-	#test_gradient()	
+	test_gradient()	
 
 	# dummy problem
-	test_mlp()
+	#test_mlp()
+	#test_mlp_dropout()
+	#test_xor()
+	#test_xor_dropout()

models/rbm.py
@@ -32,17 +32,17 @@ class rbm:
 		self.model_sample = None
 
 		if not np.any(input_mean):
-			self.input_mean     = np.zeros((1,n_visible))
+			self.input_mean = np.zeros((1,n_visible))
 		else:
-			self.input_mean     = input_mean
+			self.input_mean = input_mean
 
-		self.h_bias       = np.zeros( (1, n_hidden) )
-		self.v_bias       = np.zeros( (1, n_visible) )
-		self.W            = 1e-2 * np.random.randn( n_visible, n_hidden )
-
-		self.W_inc        = np.zeros( (n_visible, n_hidden) )
-		self.h_inc        = np.zeros( (1, n_hidden) )
-		self.v_inc        = np.zeros( (1, n_visible) )
+		self.h_bias = np.zeros( (1, n_hidden) )
+		self.v_bias = np.zeros( (1, n_visible) )
+		self.W      = 1e-2 * np.random.randn( n_visible, n_hidden )
+		
+		self.W_inc  = np.zeros( (n_visible, n_hidden) )
+		self.h_inc  = np.zeros( (1, n_hidden) )
+		self.v_inc  = np.zeros( (1, n_visible) )
 
 	def sigmoid(self, x):
 		return 1. / (1 + np.exp(-x))
@@ -157,9 +157,9 @@ class rbm:
 				Err[-1] += err
 
 				# is this use of momentum correct? should there be (1-momentum) attached to the second term?
-				self.W_inc  = self.W_inc * momentum + learning_rate * (W_grad - weight_decay*self.W)
-				self.h_inc  = self.h_inc * momentum + learning_rate * h_grad
-				self.v_inc  = self.v_inc * momentum + learning_rate * v_grad								
+				self.W_inc  = self.W_inc * momentum + (1-momentum) * learning_rate * (W_grad - weight_decay * self.W / float(n_batches))
+				self.h_inc  = self.h_inc * momentum + (1-momentum) * learning_rate * h_grad
+				self.v_inc  = self.v_inc * momentum + (1-momentum) * learning_rate * v_grad								
 
 				self.W      += self.W_inc
 				self.h_bias += self.h_inc